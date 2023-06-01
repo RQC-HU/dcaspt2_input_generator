7bd084939ec6395e0aad9fb18f7b6d04c5260804
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-06-01 14:17:40 +0900

    Load DIRAC output

--- main.py
+++ main.py
@@ -1,86 +1,114 @@
+import os
+import subprocess
+import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QTableView, QMessageBox
-from PySide6.QtGui import QStandardItemModel, QStandardItem
-import csv
-from qt_material import apply_stylesheet
+from PySide6.QtGui import QColor, QAction, QDragEnterEvent
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QMenu, QFileDialog, QMessageBox, QInputDialog
+import qt_material
+
+
+class TableWidget(QTableWidget):
+    def __init__(self):
+        super().__init__()
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
+        self.load_output("data.out")
+
+    def load_output(self, file_path):
+        with open(file_path, newline='') as output:
+            out = output.readlines()
+            # output is space separated file
+            rows = [line.split() for line in out]
+            len_row = len(rows)
+            len_column = max(len(row) for row in rows) if len_row > 0 else 0
+            self.setRowCount(len_row)
+            self.setColumnCount(len_column)
+            for row in range(len_row):
+                for column in range(len_column):
+                    try:
+                        text = rows[row][column]
+                    except IndexError:
+                        text = ""
+                    item = QTableWidgetItem(text)
+                    self.setItem(row, column, item)
+
+    def show_context_menu(self, position):
+        menu = QMenu()
+        pale_blue_action = QAction("Pale Blue", self)
+        pale_green_action = QAction("Pale Green", self)
+        pale_pink_action = QAction("Pale Pink", self)
+        pale_yellow_action = QAction("Pale Yellow", self)
+        pale_blue_action.triggered.connect(lambda: self.change_background_color(QColor("#D3E8EB")))
+        pale_green_action.triggered.connect(lambda: self.change_background_color(QColor("#D5ECD4")))
+        pale_pink_action.triggered.connect(lambda: self.change_background_color(QColor("#F4D9D9")))
+        pale_yellow_action.triggered.connect(lambda: self.change_background_color(QColor("#FDF4CD")))
+        menu.addAction(pale_blue_action)
+        menu.addAction(pale_green_action)
+        menu.addAction(pale_pink_action)
+        menu.addAction(pale_yellow_action)
+        menu.exec(self.viewport().mapToGlobal(position))
+
+    def change_background_color(self, color):
+        indexes = self.selectedIndexes()
+        rows = set()
+        for index in indexes:
+            rows.add(index.row())
+        for row in rows:
+            for column in range(self.columnCount()):
+                self.item(row, column).setBackground(color)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.table_view = QTableView()
-        self.setCentralWidget(self.table_view)
+        # Add drag and drop functionality
+        self.setAcceptDrops(True)
+        # Show the header bar
+        self.menu_bar = self.menuBar()
+        self.file_menu = self.menu_bar.addMenu("File")
+        self.open_action = QAction("Open", self)
+        self.open_action.triggered.connect(self.select_file)
+        self.file_menu.addAction(self.open_action)
 
-        # Create a model to hold the table data
-        self.model = QStandardItemModel()
-        self.table_view.setModel(self.model)
+    def select_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "SELECT A DIRAC OUTPUT FILE", "", "Output file (*.out)")
+        if file_path:
+            molecule_name, ok = self.question_molecule(file_path)
+            if not ok:
+                return
+            # Run sum_dirac_defcoef subprocess
+            self.run_sum_dirac_defcoef(file_path, molecule_name)
+            self.dropEvent()
 
-        # Load and display CSV data
-        self.load_csv_data('data.csv')
+    def question_molecule(self, file_path):
+        # Show a question message box that allow the user to write the molecule name
+        molecule_name, ok = QInputDialog.getText(self, "Molecule name", "Enter the molecule name that you calculated using DIRAC:")
+        return molecule_name, ok
 
-    def load_csv_data(self, filename):
-        with open(filename, 'r') as file:
-            reader = csv.reader(file)
-            data = list(reader)
+    def run_sum_dirac_defcoef(self, file_path, molecule_name):
+        current_dir = os.getcwd()
+        sum_dirac_defcoef_path = os.path.join(current_dir, "summarize_dirac_dfcoef_coefficients", "sum_dirac_dfcoef")
+        process = subprocess.run(f"python {sum_dirac_defcoef_path} -i {file_path} -m {molecule_name} -d 3 -c > data.out", shell=True)
+        # Check the status of the subprocess named process
+        if process.returncode != 0:
+            QMessageBox.critical(self, "Error", f"An error has ocurred while running the sum_dirac_dfcoef program. Please, check the output file. path: {file_path}\nExecuted command: {sum_dirac_defcoef_path} -i {file_path} -m {molecule_name} -d 3 -c")
 
-        # Set the number of rows and columns in the model
-        self.model.setRowCount(len(data))
-        self.model.setColumnCount(len(data[0]))
+    def dragEnterEvent(self, event: QDragEnterEvent) -> None:
+        if (event.mimeData().hasText()):
+            event.accept()
 
-        style_sheet = """
-            QTableView {
-                background-color: #D3E8EB;
-                selection-background-color: #A3C4C7;
-            }
+    def dropEvent(self, event="") -> None:
+        self.table_widget = TableWidget()
+        self.setCentralWidget(self.table_widget)
 
-            QTableView::item {
-                border: 1px solid #D3E8EB;
-                color: #333;
-                padding-left: 5px;
-                padding-right: 5px;
-            }
 
-        """
-        self.table_view.setStyleSheet(style_sheet)
-        # Don't allow editing of cells
-        self.table_view.setEditTriggers(QTableView.NoEditTriggers)  # type: ignore
-
-        # Set the data in each cell of the model
-        for row, row_data in enumerate(data):
-            for column, cell_data in enumerate(row_data):
-                item = QStandardItem(cell_data)
-                self.model.setItem(row, column, item)
-
-                # Set the alignment for each cell
-                alignCenter = getattr(Qt, 'AlignCenter')
-                item.setTextAlignment(alignCenter)
-
-        # Resize the columns to fit the content
-        self.table_view.resizeColumnsToContents()
-
-        # 1st column width requires more space
-        self.table_view.setColumnWidth(0, 50)
-
-    def cell_clicked(self, row, column):
-        row = self.model.item(row)
-        column = self.model.item(column)
-        cell_data = row.text()
-        # Show a message box with the cell text
-        msg_box = QMessageBox()
-        msg_box.setWindowTitle("Cell Clicked")
-        msg_box.setText(f"Clicked Cell: {cell_data}")
-        msg_box.setStandardButtons(QMessageBox.Ok)  # type: ignore
-        msg_box.setParent(self)
-        msg_box.setWindowModality(Qt.WindowModal)  # type: ignore
-        msg_box.setGeometry(self.geometry().center().x() - 150, self.geometry().center().y() - 75, 300, 150)
-        msg_box.exec()
-
-if __name__ == '__main__':
-    app = QApplication([])
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # qt_material.apply_stylesheet(app, theme='dark_teal.xml') # 'dark_teal.xml
+    # stylesheet = app.styleSheet()
+    # app.setStyleSheet(stylesheet + "QTableView {background-color: #514;}")
     window = MainWindow()
-    # window size is 1280x720
     window.resize(1280, 720)
-    apply_stylesheet(app, theme='dark_teal.xml')
     window.show()
-    app.exec()
+    sys.exit(app.exec())
