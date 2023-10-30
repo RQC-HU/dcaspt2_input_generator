8065409a0aa45cb7f468999264dc642d10a9d417
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-10-30 09:22:21 +0900

    fix import path

--- src/dcaspt2_input_generator/__main__.py
+++ src/dcaspt2_input_generator/__main__.py
@@ -2,6 +2,6 @@ import sys
 
 
 if __name__ == "__main__":
-    from dcaspt2_input_generator.dcaspt2_input_generator import main
+    from .dcaspt2_input_generator import main
 
     sys.exit(main())
