22c3a1af218336d2dd02a209d3cd0ecefbd4f1a2
Kohei Noda <ponserviceregist@gmail.com>
2025-08-28 20:38:23 +0900

    controller: widget_controller.py: drop IVO specific input handler

--- src/dcaspt2_input_generator/controller/widget_controller.py
+++ src/dcaspt2_input_generator/controller/widget_controller.py
@@ -11,81 +11,14 @@ class WidgetController:
         self.table_summary = table_summary
         self.table_widget = table_widget
 
-        # Connect signals and slots
-        self.table_summary.user_input.changed.connect(self.onUserInputChanged)
         # change_background_color is a slot
         self.table_widget.color_changed.connect(self.onTableWidgetColorChanged)
-
-    def handleIVOInput(self):
-        """Create standard input for IVO"""
-
-        # Create info for standard IVO input
-        # E1g,u or E1?
-        is_gerade_ungerade = True if table_data.header_info.spinor_num_info.keys() == {"E1g", "E1u"} else False
-        if is_gerade_ungerade:
-            nocc = {"E1g": 0, "E1u": 0}
-            nvcut = {"E1g": 0, "E1u": 0}
-        else:
-            nocc = {"E1": 0}
-            nvcut = {"E1": 0}
-        act = 0
-        sec = 0
-        rem_electrons = table_data.header_info.electron_number
-        row_count = self.table_widget.rowCount()
-        for row in range(row_count):
-            item = self.table_widget.item(row, 0)
-            color = item.background()
-            sym_str = item.text()
-
-            # nocc, nvcut
-            if rem_electrons > 0:
-                nocc[sym_str] += 1
-            elif color != colors.not_used.color:
-                # Reset nvcut
-                for k in nvcut.keys():
-                    nvcut[k] = 0
-            else:
-                nvcut[sym_str] += 1
-
-            # act, sec
-            if color == colors.not_used.color:
-                pass
-            elif rem_electrons > 0:
-                act += 2
-            else:
-                sec += 2
-            rem_electrons -= 2
-
-        # Create standard IVO input
-        output = ".ninact\n0\n"
-        output += f".nact\n{act}\n"
-        output += f".nsec\n{sec}\n"
-        output += f".nelec\n{act}\n"
-        if is_gerade_ungerade:
-            output += f".noccg\n{nocc['E1g']}\n.noccu\n{nocc['E1u']}\n"
-            output += "" if sum(nvcut.values()) == 0 else f".nvcutg\n{nvcut['E1g']}\n.nvcutu\n{nvcut['E1u']}\n"
-        else:
-            output += f".nocc\n{nocc['E1']}\n"
-            output += "" if sum(nvcut.values()) == 0 else f".nvcut\n{nvcut['E1']}\n"
-        output += f".totsym\n{self.table_summary.user_input.totsym_number.get_value()}\n"
-        output += f".diracver\n{self.table_summary.user_input.dirac_ver_number.get_value()}\n"
-        if table_data.header_info.moltra_scheme is not None:
-            output += f".scheme\n{table_data.header_info.moltra_scheme}\n"  # Explicitly set MOLTRA scheme.
-        output += ".subprograms\nIVO\n"
-        output += ".end\n"
-
-        # Save standard IVO input (replace active.ivo.inp)
-        with open(dir_info.ivo_input_path, "w") as f:
-            f.write(output)
-
-    def onUserInputChanged(self):
-        self.handleIVOInput()
 
     def onTableWidgetColorChanged(self):
         def get_max_mem_str(estimated_max_mem: int) -> str:
             kb = 1024
-            mb = kb ** 2
-            gb = kb ** 3
+            mb = kb**2
+            gb = kb**3
             if estimated_max_mem < kb:  # byte
                 return f"{estimated_max_mem} byte"
             elif estimated_max_mem < mb:  # KB
@@ -165,10 +98,16 @@ class WidgetController:
             inact = color_count["inactive"]
             act = color_count["ras1"] + color_count["active, ras2"] + color_count["ras3"]
             sec = color_count["secondary"]
-            inttwo = 8 * ((inact + act) ** 4) * (2 if table_data.header_info.point_group == "C1" else 1)  # inttwr, inttwi # noqa E501
-            inttwo_f1_f2 = 8 * (sec**2 * (inact + act) ** 2) * (4 if table_data.header_info.point_group == "C1" else 2)  # inttwr_f1, inttwi_f1, inttwr_f2, inttwi_f2 # noqa E501
+            inttwo = (
+                8 * ((inact + act) ** 4) * (2 if table_data.header_info.point_group == "C1" else 1)
+            )  # inttwr, inttwi # noqa E501
+            inttwo_f1_f2 = (
+                8 * (sec**2 * (inact + act) ** 2) * (4 if table_data.header_info.point_group == "C1" else 2)
+            )  # inttwr_f1, inttwi_f1, inttwr_f2, inttwi_f2 # noqa E501
             indkl = (8 * (inact + act + sec) ** 2) * 2  # indk, indl
-            rkl = (8 * (inact + act + sec) ** 2) * (2 if table_data.header_info.point_group == "C1" else 1)  # rklr, rkli # noqa E501
+            rkl = (8 * (inact + act + sec) ** 2) * (
+                2 if table_data.header_info.point_group == "C1" else 1
+            )  # rklr, rkli # noqa E501
             estimated_max_mem = inttwo + inttwo_f1_f2 + indkl + rkl
             if estimated_max_mem < 0:
                 estimated_max_mem = 0
@@ -182,5 +121,3 @@ class WidgetController:
 
         # Reload the input
         self.table_summary.update()
-
-        self.handleIVOInput()
